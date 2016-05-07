c35075bfdd91c2ca0b6ad17840dee06089038ec0
tom <tom.zhu1119@gmail.com>
2016-05-07 20:47:28 +0800

    react-router

--- base-react/src/js/client.js
+++ base-react/src/js/client.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
-import Layout from './components/Layout';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 const root = document.querySelector('#root');
 
-ReactDom.render(<Layout />,root);
+import Layout from './pages/Layout';
+import Archives from './pages/Archives';
+import Settings from './pages/Settings';
+import Featured from './pages/Featured';
+ReactDom.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Featured}></IndexRoute>
+      <Route path="archives(/:artive)" name="archives" component={Archives}></Route>
+      <Route path="settings" name="settings" component={Settings}></Route>
+    </Route>
+  </Router>
+,root);
+// ReactDom.render(<Layout/>,root);
+/* import Layout from './components/Layout';
+ReactDom.render(<Layout />,root); */
+
+/* import Archives from './pages/Archives';
+import Featured from './pages/Featured';
+import Layout from './pages/Layout';
+import Settings from './pages/Settings';
+
+ReactDom.render(
+  <Router history={hasHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Featured}></IndexRoute>
+       <Route path="archives(/:artive)" name="archives" component={Archives}></Route>
+      <Route path="settings" name="settings" component={Settings}></Route>
+    </Route>
+  </Router>
+)
+ */
--- base-react/webpack.config.js
+++ base-react/webpack.config.js
@@ -1,4 +1,5 @@
 var debug = process.env.NODE_ENV != 'production';
+// var debug = false;
 var path = require('path');
 var webpack = require('webpack');
 
@@ -17,7 +18,8 @@ module.exports = {
         exclude:/node_modules/,
         loader:'babel',
         query:{
-          presets:['react','es2015','stage-0']
+          presets:['react','es2015','stage-0'],
+          plugins:['react-html-attrs','transform-class-properties','transform-decorators-legacy']
         }
       }
     ]
