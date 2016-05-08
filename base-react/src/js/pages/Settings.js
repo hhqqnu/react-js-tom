a7dfc342f9418447284077177da03609057879a8
tom <tom.zhu1119@gmail.com>
2016-05-08 14:42:29 +0800

    form'

--- base-react/src/js/pages/Settings.js
+++ base-react/src/js/pages/Settings.js
@@ -3,7 +3,20 @@ import React from 'react';
 export default class Settings extends React.Component {
   render(){
     return (
-      <h1>Settings</h1>
+      <div>
+        <h1>Settings</h1>
+        <label>
+          <input type="radio" name="opt" defaultChecked /> Option 1
+        </label>
+        <label>
+          <input type="radio" name="opt" /> Option 2
+        </label>
+        <select defaultValue="C">
+          <option value="A">Apple</option>
+          <option value="B">Banana</option>
+          <option value="C">Cranberry</option>
+        </select>
+      </div>
     );
   }
 }
